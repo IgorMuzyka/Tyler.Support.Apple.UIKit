3ea0a138b0a4e09e32d1edccf104b7ea696d7820
Igor Muzyka <imuz@ciklum.com>
2018-11-29 17:03:20 +0200

    Updated UIKitViewType.swift

--- Sources/TylerSupportAppleUIKit/UIKitViewType.swift
+++ Sources/TylerSupportAppleUIKit/UIKitViewType.swift
@@ -1,10 +1,8 @@
 
 public enum UIKitViewType: StringLiteralType {
 
-    #warning("this might be incorrect as of Xcode 10 and new SDK bundled with it")
-
-	case view = "__ObjC.UIView.Type"
-	case button = "__ObjC.UIButton.Type"
-	case label = "__ObjC.UILabel.Type"
-	case scrollView = "__ObjC.UIScrollView.Type"
+	case view = "__C.UIView.Type"
+	case button = "__C.UIButton.Type"
+	case label = "__C.UILabel.Type"
+	case scrollView = "__C.UIScrollView.Type"
 }
